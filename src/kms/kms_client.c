e3702f147ff5462a03299115f0e7765715a37976
dec05eba <dec05eba@protonmail.com>
2023-04-08 07:12:45 +0200

    Use flatpak-spawn in flatpak

--- src/kms/kms_client.c
+++ src/kms/kms_client.c
@@ -142,10 +142,10 @@ int gsr_kms_client_init(gsr_kms_client *self, const char *card_path, const char 
             const char *args[] = { server_filepath, self->socket_path, NULL };
             execvp(args[0], (char *const*)args);
         } else if(inside_flatpak) {
-            const char *args[] = { "pkexec", server_filepath, self->socket_path, NULL };
+            const char *args[] = { "flatpak-spawn", "--host", "pkexec", server_filepath, self->socket_path, NULL };
             execvp(args[0], (char *const*)args);
         } else {
-            const char *args[] = { "flatpak-spawn", "--host", "pkexec", server_filepath, self->socket_path, NULL };
+            const char *args[] = { "pkexec", server_filepath, self->socket_path, NULL };
             execvp(args[0], (char *const*)args);
         }
         perror("execvp");

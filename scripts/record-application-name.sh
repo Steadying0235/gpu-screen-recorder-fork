540ad510f6bd479d678e0b3e0896dc5d112c4016
dec05eba <dec05eba@protonmail.com>
2024-01-24 22:34:01 +0100

    record window instead in script

--- scripts/record-application-name.sh
+++ scripts/record-application-name.sh
@@ -2,4 +2,4 @@
 
 window=$(xdotool selectwindow)
 window_name=$(xdotool getwindowclassname "$window" || xdotool getwindowname "$window" || echo "game")
-gpu-screen-recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor" -o "$HOME/Videos/recording/$window_name/$(date +"Video_%Y-%m-%d_%H-%M-%S.mp4")"
+gpu-screen-recorder -w "$window" -f 60 -a "$(pactl get-default-sink).monitor" -o "$HOME/Videos/recording/$window_name/$(date +"Video_%Y-%m-%d_%H-%M-%S.mp4")"
--- scripts/replay-application-name.sh
+++ scripts/replay-application-name.sh
@@ -2,4 +2,4 @@
 
 window=$(xdotool selectwindow)
 window_name=$(xdotool getwindowclassname "$window" || xdotool getwindowname "$window" || echo "game")
-gpu-screen-recorder -w screen -f 60 -c mkv -a "$(pactl get-default-sink).monitor" -r 60 -o "$HOME/Videos/replay/$window_name"
+gpu-screen-recorder -w "$window" -f 60 -c mkv -a "$(pactl get-default-sink).monitor" -r 60 -o "$HOME/Videos/replay/$window_name"

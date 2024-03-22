c691c7213798e6aeec09c44da1cce4f2725a1964
dec05eba <dec05eba@protonmail.com>
2024-03-22 01:14:12 +0100

    application name scripts: replace / and \ with _

--- scripts/record-application-name.sh
+++ scripts/record-application-name.sh
@@ -2,4 +2,5 @@
 
 window=$(xdotool selectwindow)
 window_name=$(xdotool getwindowclassname "$window" || xdotool getwindowname "$window" || echo "game")
+window_name="$(echo "$window_name" | tr '/\\' '_')"
 gpu-screen-recorder -w "$window" -f 60 -a "$(pactl get-default-sink).monitor" -o "$HOME/Videos/recording/$window_name/$(date +"Video_%Y-%m-%d_%H-%M-%S.mp4")"

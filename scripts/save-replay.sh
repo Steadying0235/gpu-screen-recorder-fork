bb6c38f5070297595d15c98b1ed72a06280cea63
dec05eba <dec05eba@protonmail.com>
2024-04-26 22:12:10 +0200

    scripts: change notify-send timeout to 1.5 seconds

--- scripts/save-replay.sh
+++ scripts/save-replay.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 killall -SIGUSR1 gpu-screen-recorder
-notify-send -t 5000 -u low -- "GPU Screen Recorder" "Replay saved"
+notify-send -t 1500 -u low -- "GPU Screen Recorder" "Replay saved"

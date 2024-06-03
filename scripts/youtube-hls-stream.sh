04db56aa8fe16e0cfb6fdf8cda7b9f6051a095c3
dec05eba <dec05eba@protonmail.com>
2024-06-03 10:28:26 +0200

    fix hls stream script

--- scripts/youtube-hls-stream.sh
+++ scripts/youtube-hls-stream.sh
@@ -2,4 +2,4 @@
 
 [ "$#" -ne 3 ] && echo "usage: youtube-hls-stream.sh <window_id> <fps> <livestream_key>" && exit 1
 active_sink="$(pactl get-default-sink).monitor"
-gpu-screen-recorder -w "$1" -c hls -f "$2" -q high -a "$active_sink" -ac aac "https://a.upload.youtube.com/http_upload_hls?cid=$3&copy=0&file=stream.m3u8"
+gpu-screen-recorder -w "$1" -c hls -f "$2" -q high -a "$active_sink" -ac aac -o "https://a.upload.youtube.com/http_upload_hls?cid=$3&copy=0&file=stream.m3u8"

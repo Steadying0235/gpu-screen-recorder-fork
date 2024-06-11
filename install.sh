e78e03891e66da24ddcc30779a1ba4fdcd0250a8
dec05eba <dec05eba@protonmail.com>
2024-06-11 17:47:19 +0200

    text

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$script_dir"
 
 [ $(id -u) -ne 0 ] && echo "You need root privileges to run the install script" && exit 1
 
-echo "Warning: this install.sh script exists for backwards compatibility. Use meson directly instead if possible"
+echo "Warning: this install.sh script is deprecated. Use meson directly instead if possible"
 
 test -d build || meson setup build
 meson configure --prefix=/usr --buildtype=release -Dsystemd=true -Dstrip=true build

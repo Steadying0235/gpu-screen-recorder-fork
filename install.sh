dc86b5b9eca73ddd8524a4744bd381fc4af890f7
dec05eba <dec05eba@protonmail.com>
2024-06-11 17:44:10 +0200

    Keep install script behavior the same as before, dont use it if you want normal meson behavior

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ cd "$script_dir"
 
 [ $(id -u) -ne 0 ] && echo "You need root privileges to run the install script" && exit 1
 
+echo "Warning: this install.sh script exists for backwards compatibility. Use meson directly instead if possible"
+
 test -d build || meson setup build
-meson configure --buildtype=release build
+meson configure --prefix=/usr --buildtype=release -Dsystemd=true -Dstrip=true build
 ninja -C build install
 
 echo "Successfully installed gpu-screen-recorder"

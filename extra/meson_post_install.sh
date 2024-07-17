e5716e69a2b104810c04b65cd382069e4affa4df
dec05eba <dec05eba@protonmail.com>
2024-07-17 22:29:24 +0200

    Remove setcap sys nice because it breaks portal :(

--- extra/meson_post_install.sh
+++ extra/meson_post_install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-setcap cap_sys_admin+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gsr-kms-server \
+/sbin/setcap cap_sys_admin+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gsr-kms-server \
     || echo "\n!!! Please re-run install as root\n"
-setcap cap_sys_nice+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gpu-screen-recorder
+
+# Cant do this because it breaks desktop portal (create session)!!!.
+# For some reason the desktop portal tries to access /proc/gpu-screen-recorder-pid/root from the portal process
+# which doesn't work because for some reason CAP_SYS_NICE on a program makes /proc/self/root not readable by other processes.
+#/sbin/setcap cap_sys_nice+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gpu-screen-recorder

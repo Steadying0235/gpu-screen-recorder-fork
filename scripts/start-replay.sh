da73b3ac6346105ad8404874b8bb54c846d4f54e
dec05eba <dec05eba@protonmail.com>
2024-07-23 01:50:06 +0200

    Remove old script, in start-replay script dont start if gpu screen recorder is already running

--- scripts/start-replay.sh
+++ scripts/start-replay.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pidof -q gpu-screen-recorder && exit 1
 video_path="$HOME/Videos"
 mkdir -p "$video_path"
 gpu-screen-recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor" -c mkv -r 30 -o "$video_path"

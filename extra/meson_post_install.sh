5a9601a8a0624aac26f7c40cdd2f057ee74e40e0
dec05eba <dec05eba@protonmail.com>
2024-08-08 22:33:05 +0200

    /sbin/setcap -> /usr/sbin/setcap

--- extra/meson_post_install.sh
+++ extra/meson_post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Needed to remove password prompt when recording a monitor on amd/intel or nvidia wayland
-/sbin/setcap cap_sys_admin+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gsr-kms-server \
+/usr/sbin/setcap cap_sys_admin+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gsr-kms-server \
     || echo "\n!!! Please re-run install as root\n"
 
 # Cant do this because it breaks desktop portal (create session)!!!.
@@ -9,4 +9,4 @@
 # which doesn't work because for some reason CAP_SYS_NICE on a program makes /proc/self/root not readable by other processes.
 # This is needed to allow gpu screen recorder to run faster than a heavy games fps, for example when trying to record at 60 fps
 # but the game drops to 45 fps in some place. That would also make gpu screen recorder drop to 45 fps unless this setcap is used.
-#/sbin/setcap cap_sys_nice+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gpu-screen-recorder
+#/usr/sbin/setcap cap_sys_nice+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gpu-screen-recorder

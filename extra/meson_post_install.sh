677526e2e2830b9fb31b7c21d949d9d2c169d738
dec05eba <dec05eba@protonmail.com>
2024-08-18 18:36:25 +0200

    Explain why portal needs access to process root file

--- extra/meson_post_install.sh
+++ extra/meson_post_install.sh
@@ -7,6 +7,10 @@
 # Cant do this because it breaks desktop portal (create session)!!!.
 # For some reason the desktop portal tries to access /proc/gpu-screen-recorder-pid/root from the portal process
 # which doesn't work because for some reason CAP_SYS_NICE on a program makes /proc/self/root not readable by other processes.
+# The reason portal reads that file might be because portal seems to have a security feature where its able to identify the
+# process and if the session token is stolen by another application then it will ignore the session token as it wasn't that
+# application that created the session token.
+# ---
 # This is needed to allow gpu screen recorder to run faster than a heavy games fps, for example when trying to record at 60 fps
 # but the game drops to 45 fps in some place. That would also make gpu screen recorder drop to 45 fps unless this setcap is used.
 #/usr/sbin/setcap cap_sys_nice+ep ${MESON_INSTALL_DESTDIR_PREFIX}/bin/gpu-screen-recorder
